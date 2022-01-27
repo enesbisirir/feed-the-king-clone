3109782818bcc1ccfeb9117fdc4d6c7428a52b5d
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-01-27 21:41:13 +0300

    update gamemanager: does not keep track of cakes spawned now

--- FeedTheKingClone/Assets/Scripts/GameManager.cs
+++ FeedTheKingClone/Assets/Scripts/GameManager.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private List<GameObject> cakes;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        cakes = CakeSpawner.GetCakesSpawned();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         
