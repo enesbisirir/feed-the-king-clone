8b05fe34ed13077a39eb42daebd60e5b674c8646
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-01-27 23:16:53 +0300

    Update Cake.cs

--- FeedTheKingClone/Assets/Scripts/Cake.cs
+++ FeedTheKingClone/Assets/Scripts/Cake.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Cake : MonoBehaviour
 {
+    // TODO: Use scriptable object?
+    [SerializeField] private float horizontalSpeed = 1f;
+    [SerializeField] private float fallingSpeed;
+
+    private Rigidbody2D rigidbody2d;
+
     public static Cake CurrentCake { get; private set; }
-    // Start is called before the first frame update
+
     void OnEnable()
     {
         CurrentCake = this;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        rigidbody2d.velocity = new Vector2(1,0) * horizontalSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Stop()
     {
-        
+        rigidbody2d.velocity = new Vector2(-1, 0) * horizontalSpeed;
+    }
+
+    public void Fall()
+    {
+        rigidbody2d.velocity = new Vector2(0, -1) * fallingSpeed;
     }
 }
