2e32b4eb988565c809c01405ad88ee7268e93df4
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-01-27 23:15:13 +0300

    add collision detection to tray

---
+++ FeedTheKingClone/Assets/Tray.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Tray : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+}
