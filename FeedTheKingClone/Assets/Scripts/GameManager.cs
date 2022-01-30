87973f011491693d8b1b829dc8b7f688c587df3c
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-01-30 12:34:57 +0300

    remove debug tool

--- FeedTheKingClone/Assets/Scripts/GameManager.cs
+++ FeedTheKingClone/Assets/Scripts/GameManager.cs
@@ -19,11 +19,6 @@ public class GameManager : MonoBehaviour
         {
             CakeCollection.CurrentCake.Fall();
         }
-        if (Input.GetMouseButtonDown(1))
-        {
-            Debug.Log("Current cake: " + CakeCollection.CurrentCake.name);
-            //Debug.Log("Previous cake: " + CakeCollection.Cakes.GetPreviousCake().name);
-        }
     }
 
     private void OnCakesChanged()
