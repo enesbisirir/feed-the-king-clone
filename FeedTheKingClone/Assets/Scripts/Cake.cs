a836756d4fc10b418e0622754362454679a367ec
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-01-30 19:56:27 +0300

    add cake states

--- FeedTheKingClone/Assets/Scripts/Cake.cs
+++ FeedTheKingClone/Assets/Scripts/Cake.cs
@@ -12,6 +12,7 @@ public class Cake : MonoBehaviour
     [SerializeField] private GameObject bottomRightCorner;
 
     private Rigidbody2D rigidbody2d;
+    private CakeState cakeState;
 
     public Action Fallen { get; internal set; }
 
@@ -27,6 +28,7 @@ public class Cake : MonoBehaviour
     {
         if (this == CakeCollection.Cakes.CurrentCake() && !collision.gameObject.CompareTag("Wall"))
         {
+            cakeState = CakeState.Stationary;
             Fallen?.Invoke();
             rigidbody2d.constraints = RigidbodyConstraints2D.FreezeAll;
         }
@@ -34,12 +36,26 @@ public class Cake : MonoBehaviour
 
     private void MoveHorizontally()
     {
+        cakeState = CakeState.MovingHorizontally;
         int randomDirection = UnityEngine.Random.Range(0, 2) * 2 - 1;
         rigidbody2d.velocity = new Vector2(randomDirection, 0) * cakeAttributes.HorizontalSpeed;
     }
 
     public void Fall()
     {
+        cakeState = CakeState.Falling;
         rigidbody2d.velocity = new Vector2(0, -1) * cakeAttributes.FallingSpeed;
     }
+
+    public CakeState State()
+    {
+        return cakeState;
+    }
+}
+
+public enum CakeState
+{
+    MovingHorizontally,
+    Falling,
+    Stationary
 }
