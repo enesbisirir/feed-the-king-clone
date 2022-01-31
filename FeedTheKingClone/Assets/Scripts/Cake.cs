fc30eac4d6137418a4dc6990443cac1115b35d02
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-01-31 22:00:53 +0300

    add ICollidable

--- FeedTheKingClone/Assets/Scripts/Cake.cs
+++ FeedTheKingClone/Assets/Scripts/Cake.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cake : MonoBehaviour
+public class Cake : MonoBehaviour, ICollidable
 {
     [SerializeField] private CakeAttributes cakeAttributes;
     [SerializeField] private GameObject topLeftCorner;
