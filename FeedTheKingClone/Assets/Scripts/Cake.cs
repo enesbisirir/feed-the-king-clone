77b9bac087a8863fa5de2b1a9cbdd34de9648a99
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-02-01 21:19:13 +0300

    remove CakeState

--- FeedTheKingClone/Assets/Scripts/Cake.cs
+++ FeedTheKingClone/Assets/Scripts/Cake.cs
@@ -12,7 +12,6 @@ public class Cake : MonoBehaviour, ICollidable
     [SerializeField] private GameObject bottomRightCorner;
 
     private Rigidbody2D rigidbody2d;
-    private CakeState cakeState;
 
     public static Action Fallen { get; internal set; }
 
@@ -28,7 +27,6 @@ public class Cake : MonoBehaviour, ICollidable
     {
         if (this == CakeCollection.Cakes.CurrentCake() && !collision.gameObject.CompareTag("Wall"))
         {
-            cakeState = CakeState.Stationary;
             Fallen?.Invoke();
             rigidbody2d.constraints = RigidbodyConstraints2D.FreezeAll;
         }
@@ -36,14 +34,12 @@ public class Cake : MonoBehaviour, ICollidable
 
     private void MoveHorizontally()
     {
-        cakeState = CakeState.MovingHorizontally;
         int randomDirection = UnityEngine.Random.Range(0, 2) * 2 - 1;
         rigidbody2d.velocity = new Vector2(randomDirection, 0) * cakeAttributes.HorizontalSpeed;
     }
 
     public void Fall()
     {
-        cakeState = CakeState.Falling;
         rigidbody2d.velocity = new Vector2(0, -1) * cakeAttributes.FallingSpeed;
     }
 
@@ -54,13 +50,4 @@ public class Cake : MonoBehaviour, ICollidable
     public float BottomLeftCorner() => bottomLeftCorner.transform.position.x;
 
     public float BottomRightCorner() => bottomRightCorner.transform.position.x;
-
-    public CakeState State() => cakeState;
-}
-
-public enum CakeState
-{
-    MovingHorizontally,
-    Falling,
-    Stationary
 }
