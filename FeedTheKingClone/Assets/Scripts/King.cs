2e2adb2c5693f2642e1a785fb680c4eb2bdcaa11
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-02-16 22:26:38 +0300

    add king script

---
+++ FeedTheKingClone/Assets/Scripts/King.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    private bool isFollowing = false;
+
+    private void Update()
+    {
+        if (isFollowing)
+        {
+            FollowTouch();
+        }
+    }
+
+    private void FollowTouch()
+    {
+        var touchPos = Camera.main.ScreenToWorldPoint(InputHandler.TouchPosition);
+        touchPos.y = transform.position.y;
+
+        transform.position = Vector2.Lerp(transform.position, touchPos, Time.deltaTime * speed);
+    }
+
+    public void SetFollowing(bool follow)
+    {
+        isFollowing = follow;
+    }
+}
