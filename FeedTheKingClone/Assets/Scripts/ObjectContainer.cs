83943b234bd7e45938b5809f6cf8bdcfb2efb28b
Enes Bişirir <36504225+enesbisirir@users.noreply.github.com>
2022-03-10 23:33:51 +0300

    add container dictionary

--- FeedTheKingClone/Assets/Scripts/ObjectContainer.cs
+++ FeedTheKingClone/Assets/Scripts/ObjectContainer.cs
@@ -4,4 +4,28 @@ using UnityEngine;
 
 public class ObjectContainer : MonoBehaviour
 {
+    [SerializeField] private CakeSpawner cakeSpawner;
+    [SerializeField] private Tray tray;
+    [SerializeField] private InputHandler inputHandler;
+    [SerializeField] private King king;
+
+    private Dictionary<string, object> components;
+
+    private void Awake()
+    {
+        components.Add("CakeSpawner", cakeSpawner);
+        components.Add("Tray", tray);
+        components.Add("InputHandler", inputHandler);
+        components.Add("King", cakeSpawner);
+    }
+
+    public new object GetComponent(string componentKey)
+    {
+        if (!components.ContainsKey(componentKey))
+        {
+            return null;
+        }
+
+        return components[componentKey];
+    }
 }
